74c05a7b8e176ba3c3ad10ab204bdef2a8a1418d
zhazgul004 <zhazgul004@gmail.com>
2020-11-08 20:09:01 +0600

    implemented methods to show, add, update, delete data from db

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	require_once ("./controller/controller01.php");
+    require_once ("./view/view01.php");
+    require_once ("./model/model01.php");
+    require_once ("./model/conf.php");
+    require_once ("./entity/customer.php");
+	
+    $mainView = new View01();
+    $model = new Model01($servername, $username, $password, $dbName);
+	$mainController = new Controller01($mainView, $model);
+	
+	$mainController->index();
+?>
